c40630691c349c9f747c1269c97ec65878934522
Otto Alex <ottoalex.mayerhoffer@gmail.com>
2019-03-21 16:47:27 +0200

    new picture

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    return """"<html> <body> <img src="https://images-na.ssl-images-amazon.com/images/I/61yoTtDxuiL._SX425_.jpg"> </body> </html>"""
 
 
 if __name__ == '__main__':
